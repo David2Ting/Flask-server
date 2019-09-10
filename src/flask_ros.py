ef1b956ca02759c55f3270d397bef4d39bef952e
David <dtin4@student.monash.edu>
2019-09-10 10:58:46 +1000

    Added custom message for old rovie

--- src/flask_ros.py
+++ src/flask_ros.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from std_msgs.msg import UInt32
 from std_msgs.msg import Int32
+from Flask_server.msg import DriveCmd
 def ros_callback(msg):
     print(msg)
 
@@ -15,7 +16,7 @@ project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, './')
 threading.Thread(target=lambda: rospy.init_node('example_node', disable_signals=True)).start() #Used to create a node in a non-interfering way
 rospy.Subscriber('/listener', Int32, ros_callback) #Create subscriber
-pub = rospy.Publisher('/talker', Int32, queue_size=10) #Create publisher
+pub = rospy.Publisher('/talker', DriveCmd, queue_size=10) #Create publisher
 
 app = Flask(__name__,template_folder=template_path) #Create app and specify template 
 socketio = SocketIO(app)  #Establish socket-io server
@@ -24,16 +25,17 @@ socketio = SocketIO(app)  #Establish socket-io server
 @socketio.on('driveEvent')
 def drive_cmd(message):
     print(message['RPM'])
-    msg = Int32()
-    msg.data = message['RPM'] #Publishes RPM key of the message dictionary
+    msg = DriveCmd()
+    msg.rpm = message['RPM'] #Publishes RPM key of the message dictionary
+    msg.steer_pct = message['steer']
     pub.publish(msg)
 
 
 @app.route('/') #Routes the home director to 
 def home():
 
-    return render_template('indexArm.html') #Uses the pre-made frontend
+    return render_template('index.html') #Uses the pre-made frontend
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
